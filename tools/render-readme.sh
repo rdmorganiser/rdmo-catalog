c2c08a02a54a822341144b2f9b55d0c4e51dd4c9
olaf michaelis <o.mic@web.de>
2021-05-20 16:10:07 +0200

    Improve render markdown script

--- tools/render-readme.sh
+++ tools/render-readme.sh
@@ -1,41 +1,63 @@
 #!/bin/bash
-
 scriptdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 basedir=$(echo "${scriptdir}" | grep -Po ".*(?=\/)")
 arr=($(find "${basedir}/shared" -regex ".*\.xml$" | sort))
 
-tmpfile="/tmp/rdmocatalogtable.tmp"
-readme_tpl="${basedir}/readme_template.md"
-readme_out="${basedir}/README.md"
+placeholder="___NEW_TABLE___"
+readme_file="${basedir}/README.md"
+temp_readme="/tmp/readme.tmp"
+temp_table="/tmp/new_table.tmp"
 
-rm -f "${tmpfile}"
+rm -f "${temp_readme}"
+rm -f "${temp_table}"
 
-function ec() {
-    echo -e "${1}" >>"${readme_out}"
+# functions
+function ett() {
+    echo -e "${1}" >>"${temp_table}"
 }
 
-c=0
-echo "Generate shared catalog table "
-for f in "${arr[@]}"; do
-    if [[ $(grep -Pc "<catalog\sdc:uri=.*>" "${f}") != 0 ]]; then
-        c=$((c + 1))
-        title=$(
-            cat "${f}" |
-                tr '\n' ' ' |
-                grep -Po "<catalog\s.*<\/catalog>" |
-                grep -Po "(?<=title lang=\"en\">).*?(?=</title)"
-        )
-        file=$(echo "${f}" | grep -Po "(?<=${basedir}\/).*")
-        echo "Process file ${file}"
-        echo "|${title}|${file}|" >>"${tmpfile}"
-    fi
-done
+function make_table() {
+    coutner=0
+    echo "Generate shared catalog table"
+    ett "\n\n## Shared catalogs"
+    ett "\n|Title|File|"
+    ett "|---|---|"
+    for f in "${arr[@]}"; do
+        if [[ $(grep -Pc "<catalog\sdc:uri=.*>" "${f}") != 0 ]]; then
+            counter=$((counter + 1))
+            title=$(
+                cat "${f}" |
+                    tr '\n' ' ' |
+                    grep -Po "<catalog\s.*<\/catalog>" |
+                    grep -Po "(?<=title lang=\"en\">).*?(?=</title)"
+            )
+            file=$(echo "${f}" | grep -Po "(?<=${basedir}\/).*")
+            echo "  Process ${file}"
+            ett "|${title}|${file}|"
+        fi
+    done
+    ett "\n${counter} catalogs shared\n"
+}
 
-cat "${readme_tpl}" >"${readme_out}"
-ec "\n\n## Shared catalogs"
-ec "\n|Title|File|"
-ec "|---|---|"
-cat "${tmpfile}" | sort >>"${readme_out}"
-ec "\n${c} catalogs shared"
+function remove_old_table() {
+    echo "Remove old table"
+    sed -ze "s/## Shared catalogs[^<]*catalogs shared/${placeholder}/g" \
+        "${readme_file}" >"${temp_readme}"
+}
 
-echo "Done"
+function insert_new_table() {
+    echo "Insert new table"
+    sed -i "/${placeholder}/r ${temp_table}" "${temp_readme}"
+}
+
+function clean_up() {
+    echo "Clean up README.md"
+    sed -i "s/${placeholder}//g" "${temp_readme}"
+    cat -s "${temp_readme}" >"${readme_file}"
+}
+
+# main
+make_table
+remove_old_table
+insert_new_table
+clean_up
