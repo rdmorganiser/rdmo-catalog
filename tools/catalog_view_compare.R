1bb90a2fad53c15a28344df938e65fd73218f76d
ClaraRosalie <68403484+ClaraRosalie@users.noreply.github.com>
2021-07-29 11:14:56 +0200

    change paths to files

--- tools/catalog_view_compare.R
+++ tools/catalog_view_compare.R
@@ -4,16 +4,16 @@ library(stringr)
 library(XML)
 
 compare <- function(catalogName, viewName, path=getwd()) {
-  views <- xmlTreeParse("views/demo-views.xml") #import views
+  views <- xmlTreeParse("rdmorganiser/views/rdmo.xml") #import views
   for(i in 1:length(views$doc$children$rdmo)) { #find specified view
     rawView <- as.character(unlist(views$doc$children$rdmo[i]))
     if(str_detect(rawView[8], viewName)) {
       view <- paste0(rawView, collapse = "") #creates character with length=1 (important for later if-condition)
     }
   }
-  catalog <- xmlTreeParse(paste0("questions/", catalogName, ".xml")) #import specified catalog
+  catalog <- xmlTreeParse(paste0("rdmorganiser/questions/", catalogName, ".xml")) #import specified catalog
   catalog <- paste0(as.character(catalog), collapse = "") #creates character with length=1
-  attributes <- xmlTreeParse("domain/rdmo.xml") #import attributes
+  attributes <- xmlTreeParse("rdmorganiser/domain/rdmo.xml") #import attributes
   attribute <- character() 
   for(i in 1:length(attributes$doc$children$rdmo)) { #filter xml file to list all attributes
     a <- data.frame(unlist(attributes$doc$children$rdmo[i]$attribute[[3]]))
