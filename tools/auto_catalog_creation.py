9c8b89de4c075ee45550366da4c54bada8bedf33
Henning Timm <henning.timm@tu-dortmund.de>
2021-08-25 14:18:37 +0200

    Update catalog creation to incorporate help texts (#79)
    * Fixed errors in XML
    
    * Removed trailing white spaces
    
    * Moved double training entry back to title attribute
    
    * Minor formatting
    
    * Formatted with black
    
    * Refactoring. Removed unused imports and variables.
    
    * Refactored catalog name list to be a dictionary.
    
    * Added help texts.
    
    * Formatted with black
    
    * Fixed errors in XML
    
    * Minor formatting
    
    * Formatted with black
    
    * Refactoring. Removed unused imports and variables.
    
    * Refactored catalog name list to be a dictionary.
    
    * Added help texts.
    
    * Formatted with black
    
    * Added updated help texts from #76. Use english texts as default.
    
    * Removed debug output

--- tools/auto_catalog_creation.py
+++ tools/auto_catalog_creation.py
@@ -3,13 +3,21 @@ import os
 import yaml
 from copy import deepcopy
 from lxml import etree
-from lxml.etree import Element
+
+# from lxml.etree import Element
+
 
 def main():
+    # Read XML catalog containing all files
     root = read_xml("all_questions.xml")
+
+    # Read in definitions for derived catalogs
+    # This contains catalog names under the 'catalogs'
+    # keyword and an entry for each attribute containing
+    # a boolean flag for each catalog key in the 'catalogs' list
     life_cycle_content = read_yaml("cat_member.yaml")
+
     cat_list = []
-    cat_list_tmp = []
     for cat_vars in life_cycle_content["catalogs"]:
         cat_list.append(make_root(cat_vars))
 
@@ -28,34 +36,44 @@ def main():
         sec_xpath = ".section[@dc:uri='" + sec_uri + "']"
         sec = root.find(sec_xpath, xml_nsmap)
 
-        for n, cat_info in enumerate(life_cycle_content["catalogs"]):
+        for n, catalog_info in enumerate(life_cycle_content["catalogs"]):
             # use path as key for check of membership to catalog
-            if life_cycle_content[question.find("path").text][cat_info[0]]:
+            # print(n, cat_info)
+            # print(question.find("path").text)
+            # print(life_cycle_content[question.find("path").text])
+            # print("\n\n")
+            catalog_key = catalog_info["key"]
+            if life_cycle_content[question.find("path").text][catalog_key]:
                 # check if qset and sec are already in catalog
-                if cat_list[n].find(qset_xpath, xml_nsmap) == None:
-                    if cat_list[n].find(sec_xpath, xml_nsmap) == None:
-                        tmp_sec = change_path(sec, cat_info[0])
+                if cat_list[n].find(qset_xpath, xml_nsmap) is None:
+                    if cat_list[n].find(sec_xpath, xml_nsmap) is None:
+                        tmp_sec = change_path(sec, catalog_key)
                         cat_list[n].append(deepcopy(tmp_sec))
-                    tmp_qset = change_path(qset, cat_info[0])
+                    tmp_qset = change_path(qset, catalog_key)
                     cat_list[n].append(deepcopy(tmp_qset))
-                tmp_question = change_path(question, cat_info[0])
+                tmp_question = change_path(question, catalog_key)
                 # sort the questions below their respective questionsets
                 cat_qset = cat_list[n].find(qset_xpath, xml_nsmap)
                 index = cat_list[n].index(cat_qset)
-                cat_list[n].insert(index+1, deepcopy(tmp_question))
+                cat_list[n].insert(index + 1, deepcopy(tmp_question))
             else:
                 continue
 
     write_catalogs(cat_list, life_cycle_content["catalogs"])
     change_uri(life_cycle_content["catalogs"])
 
+
 def write_catalogs(cat_list, name_list):
-    # takes a list of root elements and file names and writes them to the specified
-    # relative directory
+    # takes a list of root elements and file names and writes them to the
+    # specified relative directory
     for n, cat in enumerate(cat_list):
         tree = etree.ElementTree(cat)
-        tree.write("../rdmorganiser/questions/" + name_list[n][0] + ".xml",
-                    xml_declaration=True, encoding="UTF-8")
+        tree.write(
+            "../rdmorganiser/questions/" + name_list[n]["key"] + ".xml",
+            xml_declaration=True,
+            encoding="UTF-8",
+        )
+
 
 def change_path(element, name):
     # takes an etree.Element and catalog key and changes the path variable of
@@ -67,29 +85,41 @@ def change_path(element, name):
     path.text = name + path.text
     return element
 
+
 def change_uri(name_list):
-    # this is rather a workaround than a solution, but prefixes on Attributes make things
-    # unnecessary hard to access and change
+    # this is rather a workaround than a solution, but prefixes on Attributes
+    # make things unnecessary hard to access and change
     default_uri = "https://rdmorganiser.github.io/terms/questions/ua_ruhr"
-    for name in name_list:
-        cat = open("../rdmorganiser/questions/" + name[0] + ".xml", "r", encoding="UTF-8")
+    for catalog in name_list:
+        cat = open(
+            "../rdmorganiser/questions/" + catalog["key"] + ".xml",
+            "r",
+            encoding="UTF-8",
+        )
         lines = cat.readlines()
         cat.close()
-        cat = open("../rdmorganiser/questions/" + name[0] + ".xml", "w", encoding="UTF-8")
+        cat = open(
+            "../rdmorganiser/questions/" + catalog["key"] + ".xml",
+            "w",
+            encoding="UTF-8",
+        )
         for line in lines:
             if default_uri in line:
-                line = line.replace("ua_ruhr", name[0])
+                line = line.replace("ua_ruhr", catalog["key"])
             cat.write(line)
         cat.close()
 
-def make_root(cat_vars):
-    # takes a list of catalog variables [key, name] and generates a root element
-    # with a predefined name space among other informations
+
+def make_root(catalog_vars):
+    # takes a list of catalog variables [key, name] and generates a
+    # root element with a predefined name space among other information
     XHTML_NAMESPACE = "http://purl.org/dc/elements/1.1/"
-    XHTML = "{%s}" % XHTML_NAMESPACE
-    NSMAP = {"dc":XHTML_NAMESPACE} # the default namespace with prefix
-    root = etree.Element("rdmo", nsmap=NSMAP) # lxml only!
-    root.append(etree.fromstring("""
+    # XHTML = "{%s}" % XHTML_NAMESPACE  # TODO this variable is unused
+    NSMAP = {"dc": XHTML_NAMESPACE}  # the default namespace with prefix
+    root = etree.Element("rdmo", nsmap=NSMAP)  # lxml only!
+    root.append(
+        etree.fromstring(
+            """
         <catalog xmlns:dc="http://purl.org/dc/elements/1.1/" dc:uri="https://rdmorganiser.github.io/terms/questions/ua-ruhr">
 		<uri_prefix>https://rdmorganiser.github.io/terms</uri_prefix>
 		<key></key>
@@ -98,11 +128,17 @@ def make_root(cat_vars):
 		<title lang="en"></title>
 		<title lang="de"></title>
 	    </catalog>
-        """))
-    root[0][1].text = cat_vars[0]
-    root[0][4].text = cat_vars[1]
-    root[0][5].text = cat_vars[2]
+        """
+        )
+    )
+
+    root[0][1].text = catalog_vars["key"]
+    root[0][2].text = catalog_vars["help_text_en"]
+    root[0][4].text = catalog_vars["title_en"]
+    root[0][5].text = catalog_vars["title_de"]
+
     return root
+
 
 def read_yaml(file_name):
     # reads in a yaml file and returns the content of it
@@ -110,11 +146,13 @@ def read_yaml(file_name):
     content = yaml.safe_load(data_file)
     return content
 
+
 def read_xml(file_name):
     # reads in a xml file and returns it as etree.Element
     tree = etree.parse(os.path.abspath(file_name))
     root = tree.getroot()
     return root
+
 
 if __name__ == "__main__":
     main()
